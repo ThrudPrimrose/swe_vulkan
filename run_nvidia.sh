c6626c9cbeb36f98111f02a1ffc108bb0b325d5c
thrud <budanaz.yakup@gmail.com>
2020-01-22 15:30:37 +0100

    update script to have non-compile option

--- run_nvidia.sh
+++ run_nvidia.sh
@@ -4,7 +4,15 @@ echo "source ~/vulkan/1.1.130.0/setup-env.sh"
 source ~/vulkan/1.1.130.0/setup-env.sh 
 echo "export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/10_nvidia.json"
 export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/10_nvidia.json
-echo "compile and run:"
-make run 
 
+if [ $1 == c ] 
+then
+    echo "compile and run:"
+    make clean
+    make run
+else
+    echo "First->" $1
+    echo "only run:"
+    make run
+fi
 
