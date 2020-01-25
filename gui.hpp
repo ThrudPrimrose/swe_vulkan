78b74de8110f25aef2bc32325e1ee8677da880bf
thrud <budanaz.yakup@gmail.com>
2020-01-25 17:15:17 +0100

    remove includes to imgui

--- gui.hpp
+++ gui.hpp
@@ -25,13 +25,8 @@ class Gui {
     std::pair <int,int> size;
     GLFWwindow* window;
     static bool framebufferResized;
-    ImGui_ImplVulkanH_Window guiObjWindow;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    VkDescriptorPool guiDescriptorPool;
     uint32_t minImageCount =2;
-    std::vector<VkCommandPool> guiCommandPools;
-    std::vector<VkCommandBuffer> guiCommandBuffers;
-
+   
     void initWindow();
     bool shouldClose();
     MoveInfo poll();
@@ -41,16 +36,7 @@ class Gui {
     static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 
     void inqChange(int &height, int &width);
-
-    void setupImguiContext(Device dev, VkInstance instance, VkQueue guiQueue);
-    void createGuiDPool(Device dev, VkQueue guiQueue);
-    void createGuiRenderPass(Device dev);
-    void rebuild(Device dev,VkInstance instance);
-    void drawObjects(Device dev);
-    static void check_vk_result(VkResult x);
-    void FrameRender(ImGui_ImplVulkanH_Window* guiObjWindow, Device dev);
-    void createGuiCommandPool(Device dev,VkCommandPool* commandPool, VkCommandPoolCreateFlags flags);
-    void createGuiCommandBuffers(Device dev,VkCommandBuffer* commandBuffer, uint32_t commandBufferCount, VkCommandPool &commandPool);
+    void check_vk_result(VkResult x);
 
 };
 
